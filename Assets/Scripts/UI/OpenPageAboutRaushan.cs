03f03b4c5f8d5137cbd9113c5fc40d547d020368
cayrax2013 <54445011+cayrax2013@users.noreply.github.com>
2020-04-15 11:10:50 +0300

    Added wood board UI

---
+++ Assets/Scripts/UI/OpenPageAboutRaushan.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPageAboutRaushan : MonoBehaviour
+{
+    [SerializeField] private GameObject _aboutRaushanPage;
+    [SerializeField] private GameObject _aboutDevelopersPage;
+
+    private void OnMouseDown()
+    {
+        _aboutRaushanPage.SetActive(true);
+        _aboutDevelopersPage.SetActive(false);
+    }
+}
